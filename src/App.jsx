447506b7ce8ef39f9c8dd48d1d9fce1c14a0a3f4
Rohan <rohankumar5803@gmail.com>
2024-07-24 13:37:32 +0530

    create navbar.css and add some  style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div></div>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
