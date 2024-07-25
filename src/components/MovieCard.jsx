fcf478e462e9df8a4a16136adb1747bf84d71391
Rohan <rohankumar5803@gmail.com>
2024-07-25 17:06:33 +0530

    add necessary elements to movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../styles/movieCard.css'
+import React from "react";
+import "../styles/movieCard.css";
 
 function MovieCard() {
   return (
-    <div>
-
+    <div className="movie-card-div">
+      <img id="movie-image" src="../../src/assets/images/Harry_Potter_and_the_Prisoner_of_Azkaban_poster.webp" />
+      <h3 id="movie-title">Movie title</h3>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
