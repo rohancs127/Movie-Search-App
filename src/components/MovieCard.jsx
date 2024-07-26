bb18d120c61c422fa944010efde5282c3f619264
Rohan <rohankumar5803@gmail.com>
2024-07-26 15:27:02 +0530

    style movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard() {
   return (
     <div className="movie-card-div">
       <img id="movie-image" src="../../src/assets/images/Harry_Potter_and_the_Prisoner_of_Azkaban_poster.webp" />
-      <h3 id="movie-title">Movie title</h3>
+      <h3 id="movie-title">Harry Potter and the Prisoner of Azkaban</h3>
     </div>
   );
 }
