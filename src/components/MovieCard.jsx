874e6d463d231fd0476088b5228b2dfe1f27a78d
Rohan <rohankumar5803@gmail.com>
2024-07-29 23:19:03 +0530

    issues yet to be fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import "../styles/movieCard.css";
+import { useState, useEffect } from "react";
 import { API_LINK } from "../scripts/apiLinks";
 
 const url = API_LINK;
 
 function MovieCard() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) return response.json();
+        else throw new Error("Error in fetching data..");
+      })
+      .then((result) => {
+        let newData = result.results;
+        setData(newData)
+        // console.log(data);
+      });
+  }, []);
+
   return (
-    <div className="movie-card-div">
-      <img
-        id="movie-image"
-        src="../../src/assets/images/Harry_Potter_and_the_Prisoner_of_Azkaban_poster.webp"
-      />
-      <h3 id="movie-title">Harry Potter and the Prisoner of Azkaban</h3>
+    <div>
+      {
+      // console.log(data)
+      data.map((item) => (
+        <div className="movie-card-div" key={item.id}>
+          <img
+            id="movie-image"
+            src={item.poster_path}
+          />
+          <h3 id="movie-title">{item.title}</h3>
+        </div>
+      ))
+      }
     </div>
   );
 }
