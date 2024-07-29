57ce406b8fd6756ad5d3b898aa9d34ca1709ced4
Rohan <rohankumar5803@gmail.com>
2024-07-29 22:11:59 +0530

    moved links to seperate js file

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "../styles/movieCard.css";
 
-const API_LINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=23fac7a4c712bd0b79f8bf320213960a&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=23fac7a4c712bd0b79f8bf320213960a&query=";
-
 function MovieCard() {
   return (
     <div className="movie-card-div">
---
+++ src/scripts/apiLinks.js
@@ -0,0 +1,5 @@
+export const API_LINK =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=23fac7a4c712bd0b79f8bf320213960a&page=1";
+export const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+export const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=23fac7a4c712bd0b79f8bf320213960a&query=";
