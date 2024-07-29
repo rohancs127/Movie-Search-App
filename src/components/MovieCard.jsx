1f6a8ce8e6fb15953e55e9353cfc998cd535b200
Rohan <rohankumar5803@gmail.com>
2024-07-29 22:40:14 +0530

    imports api link to movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/movieCard.css";
+import { API_LINK } from "../scripts/apiLinks";
+
+const url = API_LINK;
 
 function MovieCard() {
   return (
     <div className="movie-card-div">
-      <img id="movie-image" src="../../src/assets/images/Harry_Potter_and_the_Prisoner_of_Azkaban_poster.webp" />
+      <img
+        id="movie-image"
+        src="../../src/assets/images/Harry_Potter_and_the_Prisoner_of_Azkaban_poster.webp"
+      />
       <h3 id="movie-title">Harry Potter and the Prisoner of Azkaban</h3>
     </div>
   );
