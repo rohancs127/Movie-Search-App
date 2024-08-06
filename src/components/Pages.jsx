ec38084ee3ba41434a984e75f0236599859e41b3
Rohan <rohankumar5803@gmail.com>
2024-08-06 22:06:43 +0530

    add page number tracking element

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import '../styles/pages.css'
+import "../styles/pages.css";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 function Pages() {
   const pageLimit = 500;
   return (
     <div className="page-div">
-      <div>1</div>
-      <div>
+      <div className="page-div-element">1</div>
+      <div className="page-div-element">
         <ChevronLeft />
       </div>
-      <div>
+      <div className="page-div-element" id="current-page"></div>
+      <div className="page-div-element">
         <ChevronRight />
       </div>
-      <div></div>
-      <div>{pageLimit}</div>
+      <div className="page-div-element">{pageLimit}</div>
     </div>
   );
 }
