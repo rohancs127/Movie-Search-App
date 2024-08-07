874dcdb9a1ebd158aa71d8faddf7c1f9fb039373
Rohan <rohankumar5803@gmail.com>
2024-08-07 20:58:30 +0530

    add info button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/movieCard.css";
+import { Info } from "lucide-react";
 
 function MovieCard({ movieId, movieImg, movieTitle }) {
   return (
@@ -7,6 +8,7 @@ function MovieCard({ movieId, movieImg, movieTitle }) {
       <div className="movie-card-div" key={movieId}>
         <img id="movie-image" src={movieImg} alt={movieTitle} />
         <h3 id="movie-title">{movieTitle}</h3>
+        <div id="movie-description"><Info color="#fff" size={"30px"} strokeWidth={"3px"}/></div>
       </div>
     </div>
   );
