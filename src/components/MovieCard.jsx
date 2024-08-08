984c45eb9a99d756f9014a70f80ec2821f41afb4
Rohan <rohankumar5803@gmail.com>
2024-08-08 21:50:42 +0530

    create and import movie info jsx file

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "../styles/movieCard.css";
 import { Info } from "lucide-react";
+import MovieInfo from "./MovieInfo";
 
-function MovieCard({ movieId, movieImg, movieTitle, movieDescription, releaseDate, imdb }) {
+function MovieCard({
+  movieId,
+  movieImg,
+  movieTitle,
+  movieDescription,
+  releaseDate,
+  imdb,
+}) {
   return (
     <div>
       <div className="movie-card-div" key={movieId}>
         <img id="movie-image" src={movieImg} alt={movieTitle} />
         <h3 id="movie-title">{movieTitle}</h3>
-        <div id="movie-description"><Info color="#fff" size={"30px"} strokeWidth={"3px"}/></div>
+        <div id="movie-description">
+          <Info color="#fff" size={"30px"} strokeWidth={"3px"} />
+          <MovieInfo />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/MovieInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MovieInfo() {
+  return (
+    <div></div>
+  )
+}
+
+export default MovieInfo
