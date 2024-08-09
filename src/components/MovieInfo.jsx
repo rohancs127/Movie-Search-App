8d54128c9e28bbcb36f56434944d85b9c27a9711
Rohan <rohankumar5803@gmail.com>
2024-08-09 21:21:35 +0530

    change movie-info style

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "../styles/moveInfo.css"
+import { CircleX } from "lucide-react";
 
 function MovieInfo({ movieDescription, releaseDate, imdb }) {
   return (
     <div className="movie-info">
+      <h3>Description</h3>
       <p className="description">{movieDescription}</p>
-      <h3 className="release-date">{releaseDate}</h3>
-      <h3 className="imdb">{imdb}</h3>
+      <h3>Release Date</h3>
+      <p className="release-date">{releaseDate}</p>
+      <h3>IMDB Rating</h3>
+      <p className="imdb">{imdb}</p>
+      <div className="close-btn"><CircleX /></div>
     </div>
   );
 }
